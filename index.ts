516987cb2f6a0ff8f76b4b8eadc555c2f76be62d
vladyslavJ <vladzhukov321@gmail.com>
2025-02-11 16:36:26 +0200

    emitter

--- index.ts
+++ index.ts
@@ -1,13 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import Logger from './logger/logger.ts';
-import PermissionException from './exceptions/PermissionsExceptions.ts';
-import TypeException from './exceptions/TypeExceptions.ts';
 
 const logger = new Logger();
 
-logger.info('Info msg');
-logger.warning('Warning msg');
-logger.error(new PermissionException());
-logger.error(new TypeException());
-logger.error(new TypeError('By the way'));
+logger.info('Hello World');
